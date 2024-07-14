a123c569b0a28a438d71e48c2004222cdb349ace
Tulio Minini <tulio.mminini@gmail.com>
2024-07-14 11:44:05 +0100

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
   if (gameType === 'addition') {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === 'multiply') {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -66,6 +68,8 @@ function calculateCorrectAnswer() {
 
   if (operator === '+') {
     return [operand1 + operand2, 'addition'];
+  } else if (operator === 'x') {
+    return [operand1 * operand2, 'multiply'];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -94,6 +98,10 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractionQuestion() {}
+function displaySubtractionQuestion(operand1, operand2) {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = 'x';
+}
