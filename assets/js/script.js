378db090285abc40d99bc09acfda0ddd762b77cc
Tulio Minini <tulio.mminini@gmail.com>
2024-07-14 11:31:21 +0100

    add score to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,10 +45,12 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert('Hey! You got it right! :D');
+    incrementScore();
   } else {
     alert(
       `Awwwwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`
     );
+    incrementWrongAnswer();
   }
   runGame(calculatedAnswer[1]);
 }
@@ -70,9 +72,21 @@ function calculateCorrectAnswer() {
   }
 }
 
-function incrementScore() {}
+/**
+ * gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById('score').innerText);
+  document.getElementById('score').innerText = ++oldScore;
+}
 
-function incrementWrongAnswer() {}
+/**
+ * gets the current score from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById('incorrect').innerText);
+  document.getElementById('incorrect').innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
