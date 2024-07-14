e9de4856d179c35a5980fc95024893c4f52dc26b
Tulio Minini <tulio.mminini@gmail.com>
2024-07-14 12:03:19 +0100

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === 'multiply') {
     displayMultiplyQuestion(num1, num2);
+  } else if (gameType === 'subtract') {
+    displaySubtractionQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -70,6 +72,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, 'addition'];
   } else if (operator === 'x') {
     return [operand1 * operand2, 'multiply'];
+  } else if (operator === '-') {
+    return [operand1 - operand2, 'subtract'];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -92,14 +96,29 @@ function incrementWrongAnswer() {
   document.getElementById('incorrect').innerText = ++oldScore;
 }
 
+/**
+ * displays the addition question
+ */
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractionQuestion(operand1, operand2) {}
+/**
+ * displays the subtraction question
+ */
+function displaySubtractionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent =
+    operand1 > operand2 ? operand1 : operand2; // to avoid operand2 being grater than operand1
+  document.getElementById('operand2').textContent =
+    operand1 > operand2 ? operand2 : operand1; // to avoid operand2 being grater than operand1
+  document.getElementById('operator').textContent = '-';
+}
 
+/**
+ * displays the multiply question
+ */
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
